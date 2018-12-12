a7ce41e6b97e2e678466fccb2232c209c8ebebb0
kisakyegordon <gordon56692@gmail.com>
2018-12-12 03:23:04 +0300

    add tests

--- components/Nav.spec.js
+++ components/Nav.spec.js
@@ -6,10 +6,7 @@ import Nav from './Nav';
 configure({ adapter: new Adapter() });
 
 describe("Test Results Component", () => {
-    it("tests for the snapshot", () => {
-        const wrapper = shallow(<Nav />);
-        expect(wrapper).toMatchSnapshot();
-    })
+
     it("tests for component classNames", () => {
         const wrapper = shallow(<Nav />);
         expect(wrapper.find('ul').hasClass('nav')).toBe(true);
